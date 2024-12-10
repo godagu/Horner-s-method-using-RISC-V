1ba2a0da8553464caa5987b21067fa8e6df2aa20
godagu <goda.gutparakyte@mif.stud.vu.lt>
2024-12-10 23:00:50 +0200

    initial working prototype, added user input

--- horners.s
+++ horners.s
@@ -2,10 +2,14 @@
 
 # hello pookie bear
 .data
-    msg: .asciz "Hello World!"
-    array: .word 2, 3, 4
-    x_value: .word 5
-    size: .byte 3
+    msgDegr: .asciz "Please enter the degree of the polynomial:"
+    msgCoef: .asciz "Please enter the polynomial coefficients separated by space. (e.g. for polynomial 3x^3 + 6x^2 - 4x + 8 enter '3 6 -4 8'"
+    msgX: .asciz "Please enter the x value:"
+    array: .word 0
+    x_value: .word 0
+    size: .byte 0
+    
+    buffer: .asciz "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
     
     # i need to define the pointer to array? or the whole array?
     # size?
@@ -16,15 +20,121 @@
 .text
 
 main:
-    la a0, array # load array address to a0 register
+    la a0, msgDegr #load symbol address to a0
+    li a7, 4 #system call for printing string
+    ecall
     
-    la t1, size # temporary store size value address in t1 register
-    lw a1, 0(t1) # load word (size value stored in t1) to a1 register
+    # read from input the degree of polynomial
+    li a7, 63 # system call to read
+    li a0, 0 # 0 for reading from stdin
+    la a1, buffer # load to a1 the address of the buffer
+    li a2, 2 # load to a2 the maximum bytes to read
+    ecall
+    
+    la t0, buffer # load address of buffer to t0
+    lb t0, 0(t0) # load the value from buffer (t0) to t0
+    
+    addi t0, t0, -0x2F # subtract '0' to make decimal
+    
+    la t1, size # load address of size to t1
+    sb t0, 0(t1) # store byte of t0 (input) to t1
+    
+    la a0, msgCoef #load symbol address to a0
+    li a7, 4 #system call for printing string
+    ecall
+    
+    li a7, 63
+    add a0, zero, zero
+    la a1, buffer
+    li a2, 255
+    ecall
+    
+    # eini per bufferi
+    la t3, buffer
+    
+    add t6, zero, zero
+    loop_parse_buffer:
+        lb t0, 0(t3) 
+        li t2, 0x20
+    
+        beq t0, t2, write_num_to_array
+        
+        addi t2, zero, 0x0A
+        beq t0, t2, write_num_to_array
+        
+        li t2, 0x30
+        blt t0, t2, exit_loop
+        
+        li t2, 0x39
+        blt t2, t0, exit_loop
+        
+        addi t0, t0, -0x30
+        
+        li t2, 0xA
+        mul t6, t6, t2
+        add t6, t0, t6     
+         
+    continue_loop:
+        addi t3, t3, 1
+        addi a0, a0, -1 
+        
+        bne a0, zero, loop_parse_buffer
+        
+        
+    write_num_to_array:
+        addi sp, sp, -VARIABLE_SIZE
+        
+        sw t6, 0(sp)
+        add t6, zero, zero
+        
+        addi t3, t3, 1
+        addi a0, a0, -1 
+        
+        bgtz a0, loop_parse_buffer
+        
+    # loop completed
+    loop_completed:
+    la t3, array
+    sw sp, 0(t3)
+        
+    la a0, msgX #load symbol address to a0
+    li a7, 4 #system call for printing string
+    ecall
+    
+    # read from input the degree of polynomial
+    li a7, 63 # system call to read
+    li a0, 0 # 0 for reading from stdin
+    la a1, buffer # load to a1 the address of the buffer
+    li a2, 2 # load to a2 the maximum bytes to read
+    ecall
+    
+    la t0, buffer # load address of buffer to t0
+    lb t0, 0(t0) # load the value from buffer (t0) to t0
+    
+    li t2, 0x30
+    blt t0, t2, exit_loop
+        
+    li t2, 0x40
+    blt t2, t0, exit_loop
+    
+        addi t0, t0, -0x30 # subtract '0' to make decimal
+    
+    la t1, x_value # load address of size to t1
+    sw t0, 0(t1) # store byte of t0 (input) to t1
+    
+    
+    la a0, array # load array address to a0 register
+    lw a0, 0(a0)
+    
+    la t1, size # temporary store size value 0x30address in t1 register
+    lb a1, 0(t1) # load word (size value stored in t1) to a1 register
     
     la t1, x_value # temporary store x_value address in t1 register
-    lw a2, 0(t1) # load x_value to a2 register
+    lb a2, 0(t1) # load x_value to a2 register
     
     jal ra, print # save the return address to ran and jump to label print
+    
+    # good practice to clean up the stack
     
     # exit
     li a7, 10
@@ -53,3 +163,9 @@ print:
     li a7, 1 # print an int (sys call provided by ripes)
     ecall 
     ret
+    
+ exit_loop:
+   # exit
+    li a7, 10
+    ecall 
+        # do sth not a number
